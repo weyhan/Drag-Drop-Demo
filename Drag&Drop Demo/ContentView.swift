3cf2ba183a4fdf8054117e23a191f8ca7dd7218f
weyhan <weyhan@gmail.com>
2021-10-17 15:36:21 +0800

    Initial Commit

---
+++ Drag&Drop Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drag&Drop Demo
+//
+//  Created by WeyHan Ng on 17/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drag&Drop Demo/Drag_Drop_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_Drop_DemoApp.swift
+//  Drag&Drop Demo
+//
+//  Created by WeyHan Ng on 17/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Drag_Drop_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
